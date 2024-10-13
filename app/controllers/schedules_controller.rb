bcaaa7af95c28aaf65d226ca61cd667553938f4a
unknown <kekesaihi@gmail.com>
2024-10-13 09:03:08 +0900

    詳細追加

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,35 @@
 class SchedulesController < ApplicationController
     def index
         @schedules = Schedule.all
+        @schedule_count = @schedules.count
+        @today = Date.today
+    end
+
+    # 新規作成フォームの表示
+    def new
+        @schedule = Schedule.new
+    end
+
+    def show
+        @schedule = Schedule.find(params[:id])
+    end
+
+    # 新規スケジュールの作成
+    def create
+        @schedule = Schedule.new(schedule_params)
+        if @schedule.save
+            flash[:notice] = 'スケジュールが作成されました。'
+            redirect_to schedules_path
+        else
+            flash.now[:alert] = 'スケジュールの作成に失敗しました。'
+            render :new
+        end
+    end
+
+    private
+
+    # Strong Parametersの設定
+    def schedule_params
+        params.require(:schedule).permit(:title, :start_date, :end_date, :all_day, :schedule_memo)
     end
 end
