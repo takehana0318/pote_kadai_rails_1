33058a8de5bd66ff5e3bab85b6ebfd4b7dc23888
unknown <kekesaihi@gmail.com>
2024-10-13 10:11:39 +0900

    最新

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -26,6 +26,28 @@ class SchedulesController < ApplicationController
         end
     end
 
+    def edit
+        @schedule = Schedule.find(params[:id])
+    end
+    
+    def update
+       @schedule = Schedule.find(params[:id])
+        if @schedule.update(schedule_params)
+            flash[:notice] = 'スケジュールが更新されました。'
+            redirect_to schedule_path(@schedule)
+        else
+            flash.now[:alert] = 'スケジュールの更新に失敗しました。'
+            render :edit
+        end
+    end
+
+    def destroy
+        @schedule = Schedule.find(params[:id])
+        @schedule.destroy
+        flash[:notice] = 'スケジュールが削除されました。'
+        redirect_to schedules_path
+    end
+
     private
 
     # Strong Parametersの設定
